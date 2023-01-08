1c8b7593425aa906186479f099cd2d430054acee
pratik_sontakke <pratikass488@gmail.com>
2023-01-08 19:04:44 +0530

    controller updated

---
+++ one9/src/main/java/com/one9/controller/SupplierController.java
@@ -0,0 +1,9 @@
+package com.one9.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/supplier")
+public class SupplierController {
+}
